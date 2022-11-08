abc80172697d1b32b7177fa572d6f55b62cf973b
kevintursi <kjt923@gmail.com>
2022-11-08 11:05:39 -0500

    Fixed Mongoose Endpoints

--- index.js
+++ index.js
@@ -285,6 +285,22 @@ app.post('/users/:Username/movies/:MovieID', (req, res) => {
   });
 });
 
+// Delete a movie from a user's favorite movie list
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $pull: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true }, // this line makes susre that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
 // Delete a user by username
 app.delete('/users/:Username', (req, res) => {
   Users.findOneAndRemove({ Username: req.params.Username })
