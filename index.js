f05c47435f568787748dfb52068247db9fa95d0e
kevintursi <kjt923@gmail.com>
2022-12-12 18:26:48 -0500

    Allowing "http://localhost:1234"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', "http://localhost:1234"];
 
 app.use(cors({
   origin: (origin, callback) => {
